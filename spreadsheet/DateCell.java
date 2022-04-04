470882bb6da8863f3c7a64d91efe4fde55cb63d9
raghavpuri2007 <82910597+raghavpuri2007@users.noreply.github.com>
2022-04-03 20:07:51 -0700

    Update DateCell.java

--- spreadsheet/DateCell.java
+++ spreadsheet/DateCell.java
@@ -1,10 +1,23 @@
-package spreadsheet;
-
+//VisiCalc
+//Raghav Puri
+//AP Computer Science
 public class DateCell extends Cell{
-
+	int day;
+	int month;
+	int year;
 	public DateCell(String[] arr) {
 		super(arr);
-		// TODO Auto-generated constructor stub
+		this.setDateValues(arr);
 	}
-	// stores months, days , and years
+	
+	public void setDateValues(String[] arr) {
+		String[] date = arr[2].split("/");
+		this.month = Integer.parseInt(date[0]);
+		this.day = Integer.parseInt(date[1]);
+		this.year = Integer.parseInt(date[2]);
+	}
+	
+	public String toString() {
+		return this.month + "/" + this.day + "/" + this.year;
+	}
 }
