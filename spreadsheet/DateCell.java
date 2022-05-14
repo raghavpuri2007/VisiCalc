b2cf843996aa8772cc07e3041a365c2ce4423ea6
raghavpuri2007 <82910597+raghavpuri2007@users.noreply.github.com>
2022-05-14 10:49:37 -0700

    Update DateCell.java

--- spreadsheet/DateCell.java
+++ spreadsheet/DateCell.java
@@ -1,23 +1,36 @@
 //VisiCalc
 //Raghav Puri
 //AP Computer Science
+
 public class DateCell extends Cell{
 	int day;
 	int month;
 	int year;
 	public DateCell(String[] arr) {
 		super(arr);
-		this.setDateValues(arr);
 	}
 	
-	public void setDateValues(String[] arr) {
+	public void setValues(String[] arr) {
 		String[] date = arr[2].split("/");
 		this.month = Integer.parseInt(date[0]);
 		this.day = Integer.parseInt(date[1]);
 		this.year = Integer.parseInt(date[2]);
 	}
 	
+	public static int getDay(DateCell cell) {
+		return cell.day;
+	}
+	
+	public static int getMonth(DateCell cell) {
+		return cell.month;
+	}
+	
+	public static int getYear(DateCell cell) {
+		return cell.year;
+	}
+	
 	public String toString() {
 		return this.month + "/" + this.day + "/" + this.year;
 	}
+	
 }
